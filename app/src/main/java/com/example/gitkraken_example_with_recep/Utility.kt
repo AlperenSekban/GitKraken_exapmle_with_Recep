4d8d3a4157c7e6e322df9692adf8f756455db6ae
Alperen <2016010205003@ogrenci.karabuk.edu.tr>
2020-10-08 22:33:59 +0300

    RetrofitClient update

---
+++ app/src/main/java/com/example/gitkraken_example_with_recep/Utility.kt
@@ -0,0 +1,4 @@
+package com.example.gitkraken_example_with_recep
+
+val readTimeout:Long=30
+val connectTimeout:Long=30
--- app/src/main/java/com/example/gitkraken_example_with_recep/data/apiServices/RetrofitClient.kt
+++ app/src/main/java/com/example/gitkraken_example_with_recep/data/apiServices/RetrofitClient.kt
@@ -1,4 +1,27 @@
 package com.example.gitkraken_example_with_recep.data.apiServices
 
-interface RetrofitClient {
+import com.example.gitkraken_example_with_recep.connectTimeout
+import com.example.gitkraken_example_with_recep.readTimeout
+import okhttp3.OkHttpClient
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+class RetrofitClient @Inject constructor() {
+
+    private val BaseUrl = "https://terminalcommands.herokuapp.com/"
+
+
+    fun getClient(): Retrofit {
+        val okHttpClient = OkHttpClient.Builder()
+            .readTimeout(readTimeout, TimeUnit.SECONDS)
+            .connectTimeout(connectTimeout, TimeUnit.SECONDS)
+            .build()
+
+        return Retrofit.Builder().baseUrl(BaseUrl).client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
