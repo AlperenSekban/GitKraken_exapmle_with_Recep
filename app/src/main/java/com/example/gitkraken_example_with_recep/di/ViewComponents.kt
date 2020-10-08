507490aaf6e939217514a6c9863040f279ef89c2
Alperen <2016010205003@ogrenci.karabuk.edu.tr>
2020-10-09 00:15:14 +0300

    categoryfragment

--- app/src/main/java/com/example/gitkraken_example_with_recep/di/ViewComponents.kt
+++ app/src/main/java/com/example/gitkraken_example_with_recep/di/ViewComponents.kt
@@ -1,4 +1,8 @@
 package com.example.gitkraken_example_with_recep.di
 
+import com.example.gitkraken_example_with_recep.ui.category.CategoryFragment
+
 interface ViewComponents {
+    fun injectCategoryFragment(categoryFragment: CategoryFragment)
+
 }
--- app/src/main/java/com/example/gitkraken_example_with_recep/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/example/gitkraken_example_with_recep/ui/category/CategoryFragment.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.gitkraken_example_with_recep.MvpApp
 import com.example.gitkraken_example_with_recep.R
+import com.example.gitkraken_example_with_recep.ui.base.BaseFragment
+import javax.inject.Inject
 
 
-class CategoryFragment : Fragment() {
+class CategoryFragment : BaseFragment(),CategoryfragmentMvpView {
+
+    @Inject
+    lateinit var presenter:CategoryFragmentMvpPresenter<CategoryfragmentMvpView>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_category, container, false)
+
+        var view =inflater.inflate(R.layout.fragment_category, container, false)
+
+        (requireActivity().application as MvpApp).viewComponents?.injectCategoryFragment(this)
+
+        presenter.onAttach(this)
+        presenter.initPresenter()
+
+        return view
     }
 
 
