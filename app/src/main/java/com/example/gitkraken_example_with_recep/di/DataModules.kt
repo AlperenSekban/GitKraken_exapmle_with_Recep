7dfd9a15b8e23bb8b3733da7eda402ee8264b782
Alperen <2016010205003@ogrenci.karabuk.edu.tr>
2020-10-08 23:12:45 +0300

    DataModule changed

--- app/src/main/java/com/example/gitkraken_example_with_recep/di/DataModules.kt
+++ app/src/main/java/com/example/gitkraken_example_with_recep/di/DataModules.kt
@@ -2,6 +2,12 @@ package com.example.gitkraken_example_with_recep.di
 
 import android.app.Application
 import android.content.Context
+import com.example.gitkraken_example_with_recep.data.apiServices.ApiServices
+import com.example.gitkraken_example_with_recep.data.apiServices.ApiServicesImp
+import com.example.gitkraken_example_with_recep.data.apiServices.RetrofitClient
+import com.example.gitkraken_example_with_recep.data.categoryServices.CategoryServices
+import com.example.gitkraken_example_with_recep.data.commandServices.CommandServices
+import com.example.gitkraken_example_with_recep.data.commandServices.CommandServicesImp
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -19,5 +25,21 @@ class DataModule(app:Application) {
         return context
     }
 
-    
+    @Singleton
+    @Provides
+    fun provideApiServices(categoryServices: CategoryServices,commandServices: CommandServices):ApiServices{
+        return ApiServicesImp(categoryServices,commandServices)
+    }
+
+    @Singleton
+    @Provides
+    fun provideCommadServices(retrofitClient: RetrofitClient):CommandServices{
+        return CommandServicesImp(retrofitClient)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitClient():RetrofitClient{
+        return RetrofitClient()
+    }
 }
