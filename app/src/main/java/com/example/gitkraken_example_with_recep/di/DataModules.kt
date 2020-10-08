88c7423374a7cb30e436cf456868d4e76e9dbf21
Recep <recep.arslan2@ogr.sakarya.edu.tr>
2020-10-08 23:23:57 +0300

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/example/gitkraken_example_with_recep/di/DataModules.kt
+++ app/src/main/java/com/example/gitkraken_example_with_recep/di/DataModules.kt
@@ -6,38 +6,16 @@ import com.example.gitkraken_example_with_recep.data.apiServices.ApiServices
 import com.example.gitkraken_example_with_recep.data.apiServices.ApiServicesImp
 import com.example.gitkraken_example_with_recep.data.apiServices.RetrofitClient
 import com.example.gitkraken_example_with_recep.data.categoryServices.CategoryServices
-import com.example.gitkraken_example_with_recep.data.commandServices.CommandServices
-import com.example.gitkraken_example_with_recep.data.commandServices.CommandServicesImp
+import com.example.gitkraken_example_with_recep.data.categoryServices.CategoryServicesImp
+import com.example.gitkraken_example_with_recep.ui.base.BasePresenter
+import com.example.gitkraken_example_with_recep.ui.base.MvpView
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
 @Module
-class DataModule(app:Application) {
-    private val context: Context
+class DataModule(app: Application) {
 
-    init {
-        this.context = app
-    }
 
-    @Singleton
-    @Provides
-    fun provideContext(): Context {
-        return context
-    }
 
-    @Singleton
-    @Provides
-    fun provideApiServices(
-        categoryServices: CategoryServices,
-        commandServices: CommandServices
-    ): ApiServices {
-        return ApiServicesImp(categoryServices, commandServices)
-    }
-
-    @Singleton
-    @Provides
-    fun provideCommadServices(retrofitClient: RetrofitClient): CommandServices {
-        return CommandServicesImp(retrofitClient)
-    }
 }
