04b67d6bc784a56d4f67d85ed17eb28941ed806b
Recep <recep.arslan2@ogr.sakarya.edu.tr>
2020-10-11 14:27:27 +0300

    commandListFragment is finished

--- app/src/main/java/com/example/gitkraken_example_with_recep/di/ViewComponents.kt
+++ app/src/main/java/com/example/gitkraken_example_with_recep/di/ViewComponents.kt
@@ -1,8 +1,10 @@
 package com.example.gitkraken_example_with_recep.di
 
 import com.example.gitkraken_example_with_recep.ui.category.CategoryFragment
+import com.example.gitkraken_example_with_recep.ui.commandlist.CommandListFragment
 
 interface ViewComponents {
     fun injectCategoryFragment(categoryFragment: CategoryFragment)
+    fun injectCommandListFragment(commandListFragment: CommandListFragment)
 
 }
--- app/src/main/java/com/example/gitkraken_example_with_recep/ui/commandlist/CommandListFragment.kt
+++ app/src/main/java/com/example/gitkraken_example_with_recep/ui/commandlist/CommandListFragment.kt
@@ -5,24 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.gitkraken_example_with_recep.MvpApp
 import com.example.gitkraken_example_with_recep.R
+import com.example.gitkraken_example_with_recep.data.models.CommandModel
+import com.example.gitkraken_example_with_recep.key
 import com.example.gitkraken_example_with_recep.ui.adapters.CommandAdapter
 import com.example.gitkraken_example_with_recep.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_command_list.*
 import javax.inject.Inject
 
 
 class CommandListFragment : BaseFragment(), CommandListMvpView {
     lateinit var adapter: CommandAdapter
+    lateinit var position: String
+
     @Inject
-    lateinit var presenter:CommandListMvpPresenter<CommandListMvpView>
+    lateinit var presenter: CommandListMvpPresenter<CommandListMvpView>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        position = arguments?.getString(key).toString()
+        (requireActivity().application as MvpApp).viewComponents?.injectCommandListFragment(this)
+        presenter.onAttact(this)
+        presenter.initPresenter()
+
         return inflater.inflate(R.layout.fragment_command_list, container, false)
+    }
+
+    override fun loadDataToList(response: List<CommandModel>) {
+
+        adapter.setData(response)
+        fragment_commandListFragment_rcyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayout.VERTICAL, false)
+        fragment_commandListFragment_rcyclerView.adapter = adapter
+    }
+
+    override fun takePosition(): String {
+        return position
     }
 
 }
