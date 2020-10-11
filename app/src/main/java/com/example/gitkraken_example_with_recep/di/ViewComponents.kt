9f566a57b0fe55d961fe9734cb177a4c7243d36c
Alperen <2016010205003@ogrenci.karabuk.edu.tr>
2020-10-11 15:04:41 +0300

    3

--- app/src/main/java/com/example/gitkraken_example_with_recep/di/ViewComponents.kt
+++ app/src/main/java/com/example/gitkraken_example_with_recep/di/ViewComponents.kt
@@ -3,7 +3,11 @@ package com.example.gitkraken_example_with_recep.di
 import com.example.gitkraken_example_with_recep.ui.category.CategoryFragment
 import com.example.gitkraken_example_with_recep.ui.commandlist.CommandListFragment
 import com.example.gitkraken_example_with_recep.ui.splashScreen.SplashScreenFragment
+import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
+@Component(modules = [DataModules::class])
 interface ViewComponents {
     fun injectCategoryFragment(categoryFragment: CategoryFragment)
     fun injectCommandListFragment(commandListFragment: CommandListFragment)
