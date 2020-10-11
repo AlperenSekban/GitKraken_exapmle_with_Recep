09f80a5ed8f1632589e96cc1051914f2c9c456ff
Alperen <2016010205003@ogrenci.karabuk.edu.tr>
2020-10-11 15:02:18 +0300

    2

--- app/src/main/java/com/example/gitkraken_example_with_recep/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/gitkraken_example_with_recep/ui/base/BaseFragment.kt
@@ -1,26 +1,14 @@
 package com.example.gitkraken_example_with_recep.ui.base
 
 import android.app.ProgressDialog
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
-import com.example.gitkraken_example_with_recep.R
 import com.example.gitkraken_example_with_recep.hideLoadinginUtilty
 import com.example.gitkraken_example_with_recep.showLoadingDialog
 
 abstract class BaseFragment : Fragment(),MvpView {
 lateinit var progressDialog: ProgressDialog
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_base, container, false)
-    }
 
     override fun showLoading() {
         progressDialog = showLoadingDialog(requireContext())
