faae60c433320553d46995537acbe23e1fcb01c5
Recep <recep.arslan2@ogr.sakarya.edu.tr>
2020-10-11 13:34:59 +0300

    recyclerView eklendi

--- app/src/main/java/com/example/gitkraken_example_with_recep/ui/adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/gitkraken_example_with_recep/ui/adapters/CategoryAdapter.kt
@@ -1,4 +1,8 @@
 package com.example.gitkraken_example_with_recep.ui.adapters
 
-class CategoryAdapter {
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+
+
+class CategoryAdapter(val context:Context,val sendId:SendId) {
 }
